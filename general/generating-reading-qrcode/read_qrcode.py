9110926cc4c2e332044e741503e0d0d676ea232a
AlexBulavin <45867076+AlexBulavin@users.noreply.github.com>
2022-01-15 23:34:48 +0300

    Fix bbox coordinates extraction
    All commented in code, please see there

--- general/generating-reading-qrcode/read_qrcode.py
+++ general/generating-reading-qrcode/read_qrcode.py
@@ -4,7 +4,9 @@ import sys
 filename = sys.argv[1]
 
 # read the QRCODE image
-img = cv2.imread(filename)
+#in case if QR code is not black/white it is better to convert it into grayscale
+img = cv2.imread(filename, 0)# Zero means grayscale
+img_origin = cv2.imread(filename)
 
 # initialize the cv2 QRCode detector
 detector = cv2.QRCodeDetector()
@@ -17,16 +19,19 @@ if bbox is not None:
     print(f"QRCode data:\n{data}")
     # display the image with lines
     # length of bounding box
-    n_lines = len(bbox)
+    n_lines = len(bbox[0])#Cause bbox = [[[float, float]]], we need to convert fload into int and loop over the first element of array
+    bbox1 = bbox.astype(int) #Float to Int conversion
     for i in range(n_lines):
         # draw all lines
-        point1 = tuple(bbox[i][0])
-        point2 = tuple(bbox[(i+1) % n_lines][0])
-        cv2.line(img, point1, point2, color=(255, 0, 0), thickness=2)
+        point1 = tuple(bbox1[0, [i][0]])
+        point2 = tuple(bbox1[0, [(i+1) % n_lines][0]])
+        cv2.line(img_origin, point1, point2, color=(255, 0, 0), thickness=2)
 
 
-
-# display the result
-cv2.imshow("img", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # display the result
+    cv2.imshow("img", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+else:
+    print("QR code not detected")
